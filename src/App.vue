56928403d8fbcfcd4772f1cee0fb48bcc18986a1
AlexandrKomisar <monolitalex@gmail.com>
2022-09-03 22:11:08 +0300

    delet hello

--- src/App.vue
+++ src/App.vue
@@ -39,11 +39,12 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background: #edeff5;
+}
+
+.container {
+  margin: 0 auto;
+  width: 80%;
 }
 
 .panel {
--- src/components/HomeServices.vue
+++ src/components/HomeServices.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>services</div>
+  <div class="serv">
+    <h3>services</h3>
+    <div class="serv__list"></div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +10,18 @@ export default {
   name: "HomeServices",
 };
 </script>
+
+<style lang="scss" scoped>
+.serv {
+  background: rgb(135, 133, 133);
+  width: 49%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  border-radius: 10px;
+  &__list {
+    border: solid 1px #fff;
+  }
+}
+</style>
